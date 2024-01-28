381d5735b01e411cc04f223062aa90f50b1a0a50
Kathrynne Winter <corviraptor@gmail.com>
2024-01-27 19:17:50 -0600

    commented out unused icon

--- src/components/icons/mod.rs
+++ src/components/icons/mod.rs
@@ -1,4 +1,4 @@
-pub const CORVIRAPTOR: &str = include_str!("corviraptor.svg");
+// pub const CORVIRAPTOR: &str = include_str!("corviraptor.svg"); uncomment if we use the corviraptor logo
 
 pub const COHOST: &str = include_str!("cohost.svg");
 pub const KOFI: &str = include_str!("kofi.svg");
